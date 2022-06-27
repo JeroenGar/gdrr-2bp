666bdc97eba1f9a118308db9cf6610037f747a82
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-06-27 22:47:29 +0200

    wip (compiles)

---
+++ src/optimization/listeners/solution_listener.rs
@@ -0,0 +1,56 @@
+use std::cmp::Ordering;
+use crate::core::cost::Cost;
+use crate::optimization::solutions::instance_solution::InstanceSolution;
+use crate::optimization::solutions::problem_solution::ProblemSolution;
+use crate::optimization::solutions::solution::Solution;
+
+pub struct SolutionListener<'a> {
+    best_complete_solution : Option<InstanceSolution<'a>>,
+    best_incomplete_solution : Option<InstanceSolution<'a>>,
+    cost_comparator : fn(&Cost, &Cost) -> Ordering,
+    material_limit : u64
+}
+
+
+impl<'a> SolutionListener<'a> {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn report_instance_solution(&mut self, solution : InstanceSolution<'a>){
+        match &self.best_incomplete_solution{
+            None => {
+                self.replace_best_incomplete_solution(solution)
+            },
+            Some(best_incomplete_solution) => {
+                if (self.cost_comparator)(&solution.cost(), &best_incomplete_solution.cost()) == Ordering::Less{
+                    self.replace_best_incomplete_solution(solution)
+                }
+            }
+        };
+        todo!()
+    }
+
+    pub fn report_problem_solution(&mut self, solution : ProblemSolution<'a>){
+        match &self.best_incomplete_solution{
+            None => {
+                self.replace_best_incomplete_solution(InstanceSolution::new(&solution));
+            },
+            Some(best_incomplete_solution) => {
+                if (self.cost_comparator)(&solution.cost(), &best_incomplete_solution.cost()) == Ordering::Less{
+                    self.replace_best_incomplete_solution(InstanceSolution::new(&solution));
+                }
+            }
+        };
+        todo!()
+    }
+
+    fn replace_best_incomplete_solution(&mut self, solution : InstanceSolution<'a>){
+        self.best_incomplete_solution = Some(solution);
+
+        todo!("check for complete solution")
+    }
+
+
+
+}
