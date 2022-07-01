4a99181f84270520d7497e86bc057fbb61fd89e2
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-07-01 15:39:24 +0200

    Revert "small fix"
    This reverts commit 912be4e236d4d5d827999813b90b9592b94ead13.

--- src/optimization/config.rs
+++ src/optimization/config.rs
@@ -5,9 +5,7 @@ use serde::{Deserialize, Serialize};
 pub struct Config {
     pub avg_nodes_removed: usize,
     pub blink_rate: f32,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_run_time: Option<usize>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_rr_iterations : Option<usize>,
     pub leftover_valuation_power: f32,
     pub history_length: usize,
