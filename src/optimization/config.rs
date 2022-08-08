52ece37a4f83bc11086b7404578daeb09d0af0a4
Jeroen Gardeyn <jeroen.gardeyn@hotmail.com>
2022-08-08 12:50:20 +0200

    json rename

--- src/optimization/config.rs
+++ src/optimization/config.rs
@@ -6,6 +6,7 @@ pub struct Config {
     pub avg_nodes_removed: usize,
     pub blink_rate: f32,
     pub max_run_time: Option<usize>,
+    #[serde(rename = "maxRRIterations")]
     pub max_rr_iterations: Option<usize>,
     pub leftover_valuation_power: f32,
     pub history_length: usize,
